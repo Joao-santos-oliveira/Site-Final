8cf63de8cf545cdeadfb8b9b3ce17ab443efdfda
Joao-santos-oliveira <80595573+Joao-santos-oliveira@users.noreply.github.com>
2021-06-21 07:16:26 -0300

    Quase pronto

---
+++ Html/Script.js
@@ -0,0 +1,84 @@
+function botao(){
+    let ano = document.querySelector('#Campeos')
+    let oi = document.querySelector('#oi')
+    
+    if(ano.value===""){
+     alert ('Campo vazio... Por favor digite um ano');
+    }
+    else if (ano.value==='1999'){
+        oi.innerHTML = 'O campeão da Champions de 1999 foi o Manchester United!'
+    }
+    else if (ano.value==='2000'){
+        oi.innerHTML = 'O campeão da Champions de 2000 foi o Real Madrid!'
+    } 
+    else if (ano.value==='2001'){
+        oi.innerHTML = 'O campeão da Champions de 2001 foi o Bayern de Munique!'
+    } 
+    else if (ano.value==='2002'){
+        oi.innerHTML = 'O campeão da Champions de 2002 foi o Real Madrid!'
+    } 
+    else if (ano.value==='2003'){
+        oi.innerHTML = 'O campeão da Champions de 2003 foi o Milan!'
+    } 
+    else if (ano.value==='2004'){
+        oi.innerHTML = 'O campeão da Champions de 2004 foi o Porto!'
+    } 
+    else if (ano.value==='2005'){
+        oi.innerHTML = 'O campeão da Champions de 2005 foi o Liverpool!'
+    } 
+    else if (ano.value==='2006'){
+        oi.innerHTML = 'O campeão da Champions de 2006 foi o Barcelona!'
+    } 
+    else if (ano.value==='2007'){
+        oi.innerHTML = 'O campeão da Champions de 2007 foi o Milan!'
+    } 
+    else if (ano.value==='2008'){
+        oi.innerHTML = 'O campeão da Champions de 2008 foi o Manchester United!'
+    } 
+    else if (ano.value==='2009'){
+        oi.innerHTML = 'O campeão da Champions de 2009 foi o Barcelona!'
+    } 
+    else if (ano.value==='2010'){
+        oi.innerHTML = 'O campeão da Champions de 2010 foi a Inter de Milão!'
+    } 
+    else if (ano.value==='2011'){
+        oi.innerHTML = 'O campeão da Champions de 2011 foi o Barcelona!'
+    } 
+    else if (ano.value==='2012'){
+        oi.innerHTML = 'O campeão da Champions de 2012 foi o Chelsea!'
+    } 
+    else if (ano.value==='2013'){
+        oi.innerHTML = 'O campeão da Champions de 2013 foi o Bayern de Munique!'
+    } 
+    else if (ano.value==='2014'){
+        oi.innerHTML = 'O campeão da Champions de 2014 foi o Real Madrid!'
+    } 
+    else if (ano.value==='2015'){
+        oi.innerHTML = 'O campeão da Champions de 2015 foi o Barcelona!'
+    } 
+    else if (ano.value==='2016'){
+        oi.innerHTML = 'O campeão da Champions de 2016 foi o Real Madrid!'
+    } 
+    else if (ano.value==='2017'){
+        oi.innerHTML = 'O campeão da Champions de 2017 foi o Real Madrid!'
+    } 
+    else if (ano.value==='2018'){
+        oi.innerHTML = 'O campeão da Champions de 2018 foi o Real Madrid!'
+    } 
+    else if (ano.value==='2019'){
+        oi.innerHTML = 'O campeão da Champions de 2000 foi o Liverpool!'
+    } 
+    else if (ano.value==='2020'){
+        oi.innerHTML = 'O campeão da Champions de 2020 foi o Bayern de Munique!'
+    } 
+    else if (ano.value==='2021'){
+        oi.innerHTML = 'O campeão da Champions de 2021 foi o Chelsea!'
+    } 
+    else{
+        oi.innerHTML = 'Esse ano não está disponivel no sistema, escolha um ano entre 1999 e 2021'
+    }
+}
+
+
+     
+
